3d2c8f840239f3b85779ac522401b653a3f022bd
Hanuman Kachwa <hanuman@abacus.co>
2019-09-04 17:01:28 +1000

    initial project commit

---
+++ SmartConnect/SmartConnect.h
@@ -0,0 +1,19 @@
+//
+//  SmartConnect.h
+//  SmartConnect
+//
+//  Created by Kheam Tan on 4/9/19.
+//  Copyright © 2019 Abacus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartConnect.
+FOUNDATION_EXPORT double SmartConnectVersionNumber;
+
+//! Project version string for SmartConnect.
+FOUNDATION_EXPORT const unsigned char SmartConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartConnect/PublicHeader.h>
+
+
